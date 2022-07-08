b53cf3765001c69b248a3d9e44d3ccce02b5f1c4
bus1hero <petru.cervac@gmail.com>
2022-07-08 14:50:19 +0300

    Add .nuke back to the ignored folders

--- build/Build.Docker.cs
+++ build/Build.Docker.cs
@@ -8,14 +8,44 @@ using Nuke.Common;
 partial class Build
 {
     [Parameter, Secret] readonly string DockerRegistryKey;
+    [Parameter] readonly string Tag = "latest";
+
+    private const string Server = "ghcr.io";
+    private const string UserName = "bushero";
+    private const string AppImageName = "jenkinstestapp";
+
+    private string PushableAppImageName => $"{Server}/{UserName}/{AppImageName}:{Tag}";
 
     Target LoginRegistry => _ => _
         .Requires(() => DockerRegistryKey)
         .Executes(() => DockerLogin(_ => _
-            .SetServer("ghcr.io")
-            .SetUsername("BusHero")
+            .SetServer(Server)
+            .SetUsername(UserName)
             .SetPassword(DockerRegistryKey)
     ));
+
+    Target BuildImage => _ => _
+        .DependsOn(Publish)
+        .Executes(() => DockerBuild(_ => _
+            .SetQuiet(true)
+            .SetTag(AppImageName)
+            .SetPath(Solution.JenkinsTestApp.Directory)
+        ));
+
+    Target TagImage => _ => _
+        .DependsOn(LoginRegistry)
+        .Requires(() => Tag)
+        .Executes(() => DockerTag(_ => _
+            .SetSourceImage(AppImageName)
+            .SetTargetImage(PushableAppImageName)
+        ));
+
+    Target PushImage => _ => _
+        .DependsOn(TagImage)
+        .Requires(() => Tag)
+        .Executes(() => DockerPush(_ => _
+            .SetName(PushableAppImageName)
+        ));
 }
 
 #pragma warning restore CA1822, IDE0051  // Mark members as static
--- build/Build.cs
+++ build/Build.cs
@@ -1,7 +1,11 @@
 using Nuke.Common;
 using Nuke.Common.ProjectModel;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.IO;
 
-#pragma warning disable CA1822, IDE0051 
+
+#pragma warning disable CA1822, IDE0051
 partial class Build : NukeBuild
 {
     public static int Main() => Execute<Build>(x => x.Compile);
@@ -11,23 +15,49 @@ partial class Build : NukeBuild
 
     [Solution(GenerateProjects = true)] readonly Solution Solution;
 
+    private AbsolutePath PublishDirectory => Solution.JenkinsTestApp.Directory / "bin" / "publish";
+
     Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-        });
+        .Description("Clean the project")
+        .Executes(() => DotNetClean(_ => _
+            .SetProject(Solution.JenkinsTestApp)
+            .SetConfiguration(Configuration)
+            .SetNoLogo(true)
+            .SetVerbosity(DotNetVerbosity.Quiet)
+        ));
 
     Target Restore => _ => _
-        .Executes(() =>
-        {
-        });
+        .Description("Restore the project")
+        .Executes(() => DotNetRestore(_ => _
+            .SetVerbosity(DotNetVerbosity.Quiet)
+        ));
 
     Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-        });
+        .Description("Compile the project")
+        .Executes(() => DotNetBuild(_ => _
+            .SetVerbosity(DotNetVerbosity.Quiet)
+            .SetConfiguration(Configuration)
+            .SetNoLogo(true)
+            .SetProjectFile(Solution.JenkinsTestApp)
+        ));
 
+    Target Publish => _ => _
+        .Description("Publish the project")
+        .Executes(() => DotNetPublish(_ => _
+            .SetConfiguration(Configuration)
+            .SetNoLogo(true)
+            .SetVerbosity(DotNetVerbosity.Quiet)
+            .SetProject(Solution.JenkinsTestApp)
+            .SetOutput(PublishDirectory)
+        ));
+
+    Target Test => _ => _
+        .Description("Test the project")
+        .Executes(() => DotNetTest(_ => _
+            .SetConfiguration(Configuration)
+            .SetVerbosity(DotNetVerbosity.Quiet)
+            .SetLoggers("trx")
+        ));
 
 }
 #pragma warning restore IDE0051, CA1822 // Remove unused private members
